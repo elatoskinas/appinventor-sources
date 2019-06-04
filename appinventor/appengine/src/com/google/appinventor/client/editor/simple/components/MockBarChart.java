e8060e8823ad4b765606398a4025983402cbc143
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-04 21:10:27 +0200

    Add simple Mock Bar Chart

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockBarChart.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockBarChart.java
@@ -3,19 +3,14 @@ package com.google.appinventor.client.editor.simple.components;
 import com.google.appinventor.client.editor.simple.SimpleEditor;
 import com.google.gwt.resources.client.ImageResource;
 import org.pepstock.charba.client.BarChart;
+import org.pepstock.charba.client.data.BarDataset;
 import org.pepstock.charba.client.resources.EmbeddedResources;
 import org.pepstock.charba.client.resources.ResourcesType;
 
-public final class MockBarChart extends MockVisibleComponent {
+public final class MockBarChart extends MockChart<BarChart> {
     private static final String PROPERTY_DESCRIPTION = "Description";
 
     public static final String TYPE = "BarChart";
-
-    private BarChart barChartWidget;
-
-    static {
-        ResourcesType.setClientBundle(EmbeddedResources.INSTANCE);
-    }
 
     /**
      * Creates a new instance of a Mock Bar Chart component.
@@ -25,13 +20,33 @@ public final class MockBarChart extends MockVisibleComponent {
     public MockBarChart(SimpleEditor editor) {
         super(editor, TYPE, images.image());
 
-        // Initialize Bar Chart mock Widget
-        barChartWidget = new BarChart();
+        chartWidget = new BarChart();
 
-        // Chart widget setup
-        barChartWidget.getOptions().setMaintainAspectRatio(false);
-        barChartWidget.getOptions().getTitle().setDisplay(true);
-        barChartWidget.setStylePrimaryName("ode-SimpleMockComponent");
-        initComponent(barChartWidget);
+        loadTestData();
+
+        initChart();
+    }
+
+    /**
+     * Loads a test data set for the Bar Chart widget.
+     */
+    private void loadTestData() {
+        // Initialize data set
+        BarDataset dataset = chartWidget.newDataset();
+
+        // Construct test data
+        dataset.setData(1, 7, 5, 4);
+
+        // Style settings
+        dataset.setBorderColor("black");
+        dataset.setBackgroundColor("black");
+        dataset.setBorderWidth(1);
+        dataset.setLabel("Data");
+
+        // Set the data set to the chart
+        chartWidget.getData().setDatasets(dataset);
+
+        // Set x value labels
+        chartWidget.getData().setLabels("1", "2", "3", "4");
     }
 }
